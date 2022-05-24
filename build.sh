835a0232dd770b1e23f82a1c6f5d1b9355fc80bb
Siva <cubesiva@gmail.com>
2022-05-24 19:58:59 +0100

    k8s hello app in nodejs to test setup

---
+++ build.sh
@@ -0,0 +1,18 @@
+projectid=notional-portal-349616
+namespace=apps
+gkecluster=myk8s
+zone=europe-west2-a
+
+echo "Creating cluster"
+gcloud container clusters create $gkecluster --project $projectid --zone $zone --num-nodes=1
+echo "getting credentials"
+gcloud container clusters  get-credentials $gkecluster --zone $zone --project $projectid
+docker build --tag  gcr.io/$projectid/hello .
+docker push gcr.io/$projectid/hello
+kubectl create ns apps
+
+kubectl apply -f deployment.yaml -n $namespace
+kubectl apply -f service.yaml -n $namespace
+
+
+kubectl get all -n apps
